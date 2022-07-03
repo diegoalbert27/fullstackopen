3b274dafea5d543d92317f784d50756783b02b09
dHinagas <hinagasrodriguez@gmail.com>
2022-07-03 16:31:24 -0400

    course added most components

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,6 +1,23 @@
 import React from 'react'
-import Content from './Content'
-import Header from './Header'
+
+const Header = ({ title }) => <h2>{title}</h2>
+
+const Content = ({ parts }) => {
+  const total = parts
+    .map((part) => part.exercises)
+    .reduce((a, b) => a + b, 0)
+  
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <p><b>Total of {total} exercises</b></p>
+    </div>
+  )
+}
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
 const Course = ({ course })  => {
   return (
